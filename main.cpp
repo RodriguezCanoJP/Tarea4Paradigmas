d438b5f7c9a32873df2781c2142f1a63d0704bb1
RodriguezCanoJP <jp99@estudiantec.cr>
2024-05-11 15:09:04 -0600

    inicio ventana

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <SDL.h>
+#include "constants.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+            NULL,
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
+            SDL_WINDOW_BORDERLESS
+    );
+
+    if(!window){
+        fprintf(stderr, "Error al inicalizar ventana.\n");
+        return 0;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer){
+        fprintf(stderr, "Error initializing renderer.'n");
+        return 0;
+    }
+    while(SDL_GetTicks() < 3000){
+        SDL_SetRenderDrawColor(renderer, 80, 70, 255, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
     return 0;
 }
