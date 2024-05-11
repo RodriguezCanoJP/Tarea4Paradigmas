fa8cb1ea917e5bce913185cdbe6380a5eb6fdf19
RodriguezCanoJP <jp99@estudiantec.cr>
2024-05-11 15:17:24 -0600

    separa en funciones de logica del juego, input para salir

--- constants.h
+++ constants.h
@@ -1,2 +1,5 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
+
+#define TRUE 1
+#define FALSE 0
--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 #include <SDL.h>
 #include "constants.h"
 
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+int juego_corriendo = FALSE;
 
-int main() {
-    SDL_Init(SDL_INIT_VIDEO);
+int initializa_ventana(void){
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        fprintf(stderr, "Error initializing SDL.\n");
+        return FALSE;
+    };
 
-    SDL_Window *window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
             NULL,
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
@@ -16,26 +22,58 @@ int main() {
     );
 
     if(!window){
-        fprintf(stderr, "Error al inicalizar ventana.\n");
-        return 0;
+        fprintf(stderr, "Error initializing the window./n");
+        return FALSE;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
     if(!renderer){
         fprintf(stderr, "Error initializing renderer.'n");
-        return 0;
-    }
-    while(SDL_GetTicks() < 3000){
-        SDL_SetRenderDrawColor(renderer, 80, 70, 255, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+        return FALSE;
     }
 
+    return TRUE;
+}
 
-    SDL_Delay(3000);
+void procesa_input(){
+    SDL_Event event;
+    SDL_PollEvent(&event);
 
+    switch (event.type){
+        case SDL_QUIT:
+            juego_corriendo = FALSE;
+            break;
+        case SDL_KEYDOWN:
+            if(event.key.keysym.sym == SDLK_ESCAPE){
+                juego_corriendo = FALSE;
+            }
+    }
+}
+
+void render(){
+    SDL_SetRenderDrawColor(renderer, 80, 70, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+void destruye_ventana(void){
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     SDL_Quit();
+}
+
+int main() {
+
+    juego_corriendo = initializa_ventana();
+
+    while(juego_corriendo){
+        procesa_input();
+        render();
+    }
+    destruye_ventana();
+
+
     return 0;
 }
