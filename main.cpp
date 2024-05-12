5a0e3c803ee297bfd2f34d8ceaaac4968d120353
RodriguezCanoJP <jp99@estudiantec.cr>
2024-05-12 11:25:24 -0600

    movimiento y perspectiva con colores

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 int juego_corriendo = FALSE;
 
+struct car{
+    float distance;
+    float speed;
+} car;
+
 int initializa_ventana(){
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
         fprintf(stderr, "Error initializing SDL.\n");
@@ -37,6 +42,11 @@ int initializa_ventana(){
     return TRUE;
 }
 
+void setup(){
+    car.distance=0.0f;
+    car.speed=0.0f;
+}
+
 void procesa_input(){
     SDL_Event event;
     SDL_PollEvent(&event);
@@ -49,7 +59,12 @@ void procesa_input(){
             if(event.key.keysym.sym == SDLK_ESCAPE){
                 juego_corriendo = FALSE;
             }
+        default:
+            break;
     }
+}
+void actualiza(){
+    car.distance += 1;
 }
 
 void render(){
@@ -58,7 +73,7 @@ void render(){
 
     for(int y=0; y < WINDOW_HEIGHT/2; y++){
         for(int x=0; x < WINDOW_WIDTH; x++){
-            float fPerspective = (float)y / (WINDOW_HEIGHT/2);
+            float fPerspective = (float)y/ (WINDOW_HEIGHT/2);
 
             float fPuntoMedio = 0.5f;
             float fAnchoCalle = 0.1f  + fPerspective * 0.8f;
@@ -73,13 +88,16 @@ void render(){
             int KerbsDerecha = (fPuntoMedio + fAnchoCalle ) * WINDOW_WIDTH;
 
             int row = WINDOW_HEIGHT/2 + y;
+            int color = sinf(20.0f * powf(1-fPerspective, 3) + car.distance * 0.1f) > 0.0f? 30:80;
+            int kerbsColor = sinf(20.0f * powf(1-fPerspective, 3) + car.distance * 0.3f) > 0.0f? 0:255;
+
 
             if(x >=0 && x < PastoIzquierda){
-                SDL_SetRenderDrawColor(renderer, 50, 255, 150, 255);
+                SDL_SetRenderDrawColor(renderer, color, 150, color, 56);
                 SDL_RenderDrawPoint(renderer, x, row);
             }
             if(x >= PastoIzquierda && x<KerbsIzquierda){
-                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                SDL_SetRenderDrawColor(renderer, 255, kerbsColor, kerbsColor, 255);
                 SDL_RenderDrawPoint(renderer, x, row);
             }
             if(x >= KerbsIzquierda && x<KerbsDerecha){
@@ -87,11 +105,11 @@ void render(){
                 SDL_RenderDrawPoint(renderer, x, row);
             }
             if(x >= KerbsDerecha && x<PastoDerecha){
-                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                SDL_SetRenderDrawColor(renderer, 255, kerbsColor, kerbsColor, 255);
                 SDL_RenderDrawPoint(renderer, x, row);
             }
             if(x >= PastoDerecha){
-                SDL_SetRenderDrawColor(renderer, 50, 255, 150, 255);
+                SDL_SetRenderDrawColor(renderer, color, 150, color, 56);
                 SDL_RenderDrawPoint(renderer, x, row);
             }
         }
@@ -109,9 +127,11 @@ void destruye_ventana(){
 int main() {
 
     juego_corriendo = initializa_ventana();
+    setup();
 
     while(juego_corriendo){
         procesa_input();
+        actualiza();
         render();
     }
     destruye_ventana();
