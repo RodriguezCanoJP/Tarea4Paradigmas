74bc1f747d8a6662ea772db5bc4ada552da8ced1
RodriguezCanoJP <jp99@estudiantec.cr>
2024-05-13 11:30:12 -0600

    visualizacion de curvas

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include <SDL.h>
 #include "constants.h"
 
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
 int juego_corriendo = FALSE;
 float ultimo_tiempo = 0.0f;
+float curvaturaActual = 0;
+int curvatura;
+int seccion = 0;
+int secciones[10][2] =  {{2000, 0},
+                              {1000, -1},
+                              {1000, -2},
+                              {3000, 0},
+                              {700, 1},
+                              {700, -1},
+                              {2000, 0},
+                              {1000, 1},
+                              {1000, 2},
+                              {4000, 0}};
 
 struct car{
     float distancia;
@@ -14,7 +27,7 @@ struct car{
 
 int initializa_ventana(){
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
-        fprintf(stderr, "Error initializing SDL.\n");
+        fprintf(stderr, "Error al inicializar SDL.\n");
         return FALSE;
     };
 
@@ -28,24 +41,22 @@ int initializa_ventana(){
     );
 
     if(!window){
-        fprintf(stderr, "Error initializing the window./n");
+        fprintf(stderr, "Error al inicializar la ventana.\n");
         return FALSE;
     }
-
-
 
     renderer = SDL_CreateRenderer(window, -1, 0);
     if(!renderer){
-        fprintf(stderr, "Error initializing renderer.'n");
+        fprintf(stderr, "Error al inicializar el renderer.\n");
         return FALSE;
     }
-
     return TRUE;
 }
 
 void setup(){
     car.distancia=0.0f;
     car.velocidad=0.0f;
+    curvatura = secciones[seccion][1];
 }
 
 void procesa_input(){
@@ -59,18 +70,19 @@ void procesa_input(){
         case SDL_KEYDOWN:
             switch (event.key.keysym.sym) {
                 case SDLK_UP:
-                    car.velocidad+=1.0f;
+                    car.velocidad+=5.0f;
                     break;
                 case SDLK_DOWN:
-                    car.velocidad = abs(car.velocidad - 2.0f);
+                    car.velocidad = abs(car.velocidad - 5.0f);
                     break;
                 case SDLK_ESCAPE:
                     juego_corriendo = FALSE;
                     break;
                 default:
-                    car.velocidad-=abs(car.velocidad - 0.2f);;
+                    break;
             }
         default:
+            car.velocidad = abs(car.velocidad - 1.0f);
             break;
     }
 }
@@ -82,7 +94,14 @@ void actualiza(){
     }
     float delta_time = (SDL_GetTicks() - ultimo_tiempo)/1000.0f;
     car.distancia += delta_time * car.velocidad;
+    if(car.distancia >= secciones[seccion][0]){
+        car.distancia = 0;
+        seccion +=1;
+        curvatura = secciones[seccion][1];
+    }
     ultimo_tiempo = SDL_GetTicks();
+    float diffCurvas = (curvatura - curvaturaActual)*delta_time;
+    curvaturaActual += diffCurvas;
 }
 
 void render(){
@@ -91,12 +110,15 @@ void render(){
 
     for(int y=0; y < WINDOW_HEIGHT/2; y++){
         for(int x=0; x < WINDOW_WIDTH; x++){
-            float fPerspective = (float)y/ (WINDOW_HEIGHT/2);
+            float perspectiva = (float)y/ (WINDOW_HEIGHT/2);
 
-            float puntoMedio = 0.5f;
-            float anchoCalle = 0.1f  + fPerspective * 0.8f;
+
+
+
+
+            float puntoMedio = 0.5f + curvaturaActual * pow((1-perspectiva),2);
+            float anchoCalle = 0.1f  + perspectiva * 0.8f;
             float anchoKerbs = anchoCalle * 0.15f;
-
 
             anchoCalle *= 0.5;
 
@@ -106,9 +128,8 @@ void render(){
             int KerbsDerecha = (puntoMedio + anchoCalle ) * WINDOW_WIDTH;
 
             int row = WINDOW_HEIGHT/2 + y;
-            int color = sinf(20.0f * powf(1-fPerspective, 3) + car.distancia * 0.1f) > 0.0f? 30:80;
-            int kerbsColor = sinf(30.0f * powf(1-fPerspective, 3) + car.distancia * 0.3f) > 0.0f? 0:255;
-
+            int color = sinf(20.0f * powf(1-perspectiva, 3) + car.distancia * 0.1f) > 0.0f? 30:80;
+            int kerbsColor = sinf(30.0f * powf(1-perspectiva, 3) + car.distancia * 0.3f) > 0.0f? 0:255;
 
             if(x >=0 && x < PastoIzquierda){
                 SDL_SetRenderDrawColor(renderer, color, 150, color, 56);
@@ -133,6 +154,9 @@ void render(){
         }
     }
 
+    SDL_SetRenderDrawColor(renderer, 200, 20, 20, 100);
+    SDL_Rect sprite = {350, 500, 100, 50};
+    SDL_RenderFillRect(renderer, &sprite);
     SDL_RenderPresent(renderer);
 }
 
@@ -153,7 +177,5 @@ int main() {
         render();
     }
     destruye_ventana();
-
-
     return 0;
 }
