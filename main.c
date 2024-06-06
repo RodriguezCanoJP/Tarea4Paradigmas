0582a07ec541fa0e00cca8c180be3ed8e64eb0d0
JuanPablo <jp99@estudiantec.cr>
2024-06-05 18:21:11 -0600

    bug en render

--- main.c
+++ main.c
@@ -87,7 +87,7 @@ int initializa_ventana(){
     newtwork_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
+    server_address.sin_port = htons(44553);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     int connection_status = connect(newtwork_socket, (struct sockaddr *)&server_address, sizeof(server_address));
@@ -229,7 +229,6 @@ void render(){
     spriteRect.x = car.posx;
     spriteRect.y = CAR_Y_POS;
     SDL_RenderCopy(renderer, spriteTexture, NULL, &spriteRect);
-    SDL_RenderPresent(renderer);
     SDL_RenderPresent(renderer);
 }
 
